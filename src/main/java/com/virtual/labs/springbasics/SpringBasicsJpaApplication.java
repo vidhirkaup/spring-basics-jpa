957498e6ce7a90672d348d93a63d84279761857b
Vidhir Kaup <vk4web@gmail.com>
2019-04-29 20:46:38 -0400

    Initial commit

---
+++ src/main/java/com/virtual/labs/springbasics/SpringBasicsJpaApplication.java
@@ -0,0 +1,13 @@
+package com.virtual.labs.springbasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicsJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBasicsJpaApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/virtual/labs/springbasics/entity/Course.java
@@ -0,0 +1,34 @@
+package com.virtual.labs.springbasics.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Course {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    protected Course() {
+    }
+
+    public Course(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
