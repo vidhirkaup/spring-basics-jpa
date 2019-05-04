7110a4253ce8a22835cc44e12a6fcfc00fc20498
Vidhir Kaup <vk4web@gmail.com>
2019-05-04 06:36:49 -0400

    entiity manager - maintains and tracks updates in a transaction

--- src/main/java/com/virtual/labs/springbasics/SpringBasicsJpaApplication.java
+++ src/main/java/com/virtual/labs/springbasics/SpringBasicsJpaApplication.java
@@ -1,17 +1,27 @@
 package com.virtual.labs.springbasics;
 
+import com.virtual.labs.springbasics.repository.CourseRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBasicsJpaApplication {
+public class SpringBasicsJpaApplication implements CommandLineRunner {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    CourseRepository courseRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBasicsJpaApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        courseRepository.playWithEntityManager();
+    }
 }
--- src/main/java/com/virtual/labs/springbasics/repository/CourseRepository.java
+++ src/main/java/com/virtual/labs/springbasics/repository/CourseRepository.java
@@ -1,6 +1,8 @@
 package com.virtual.labs.springbasics.repository;
 
 import com.virtual.labs.springbasics.entity.Course;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,6 +12,8 @@ import javax.persistence.EntityManager;
 @Repository
 @Transactional
 public class CourseRepository {
+
+    Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     EntityManager entityManager;
@@ -30,6 +34,14 @@ public class CourseRepository {
     public void deleteById(Long id) {
         Course course = findById(id);
         entityManager.remove(course);
+    }
+
+    public void playWithEntityManager() {
+        Course course = new Course("boot in 10 steps");
+        entityManager.persist(course);
+        logger.info("{}", course);
+        course.setName("BOOT IN 10 STEPS");
+        logger.info("{}", course);
     }
 
 }
